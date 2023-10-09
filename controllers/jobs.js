1f0bee63b5c4b9f702e83912fef1ec57ed1aa9fa
leopoldkwok <leopoldkwok@gmail.com>
2023-10-09 13:21:40 +0100

    Routes

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,20 +1,21 @@
 const getAllJobs = async (req, res) => {
   res.send('get all jobs');
 };
+
 const getJob = async (req, res) => {
   res.send('get job');
 };
+
 const createJob = async (req, res) => {
   res.send('create job');
 };
+
 const updateJob = async (req, res) => {
   res.send('update job');
 };
+
 const deleteJob = async (req, res) => {
   res.send('delete job');
-};
-const login = async (req, res) => {
-  res.send('login user');
 };
 
 module.exports = {
